203b748e719842f725acab3f6f8b8e79f316afdf
Robert Hudspeth <hudspero@users.noreply.github.com>
2019-10-21 12:49:39 -0700

    Included actual project files

---
+++ code.js
@@ -0,0 +1,84 @@
+function Reg() {
+	document.getElementById("mainTable").style.display = "none";
+	document.getElementById("RegTable").style.display = "block";
+}
+
+function Acc() {
+	document.getElementById("mainTable").style.display = "none";
+	document.getElementById("AccTable").style.display = "block";
+}
+
+	function ACurrent() {
+		document.getElementById("AccTable").style.display = "none";
+		document.getElementById("ACurrent").style.display = "block";
+	}
+
+		function ChangeONIDPW() {
+			document.getElementById("ACurrent").style.display = "none";
+			document.getElementById("ChangeONIDPW").style.display = "block";
+		}
+function Hdw() {
+	document.getElementById("mainTable").style.display = "none";
+	document.getElementById("HdwTable").style.display = "block";
+}
+
+function Stw() {
+	document.getElementById("mainTable").style.display = "none";
+	document.getElementById("StwTable").style.display = "block";
+}
+
+function MD() {
+	document.getElementById("mainTable").style.display = "none";
+	document.getElementById("MDTable").style.display = "block";
+}
+
+function PM() {
+	document.getElementById("mainTable").style.display = "none";
+	document.getElementById("PMTable").style.display = "block";
+}
+
+	function PPrinters() {
+		document.getElementById("PMTable").style.display = "none";
+		document.getElementById("PPrinters").style.display = "block";
+	}
+
+		function PersonalPrinters() {
+			document.getElementById("PPrinters").style.display = "none";
+			document.getElementById("PersonalPrinters").style.display = "block";
+		}
+	
+function NW() {
+	document.getElementById("mainTable").style.display = "none";
+	document.getElementById("NWTable").style.display = "block";
+}
+
+	function NWireless() {
+		document.getElementById("NWTable").style.display = "none";
+		document.getElementById("NWireless").style.display = "block";
+	}
+	
+		function SetupWirelessNW() {
+			document.getElementById("NWireless").style.display = "none";
+			document.getElementById("SetupWirelessNW").style.display = "block";
+
+		}
+
+function GoBack() {
+}
+
+function StartOver() {
+	document.getElementById("mainTable").style.display = "block";
+	document.getElementById("RegTable").style.display = "none";
+	document.getElementById("AccTable").style.display = "none";
+		document.getElementById("ACurrent").style.display = "none";
+			document.getElementById("ChangeONIDPW").style.display = "none";
+	document.getElementById("HdwTable").style.display = "none";
+	document.getElementById("StwTable").style.display = "none";
+	document.getElementById("MDTable").style.display = "none";
+	document.getElementById("PMTable").style.display = "none";
+		document.getElementById("PPrinters").style.display = "none";
+			document.getElementById("PersonalPrinters").style.display = "none";
+	document.getElementById("NWTable").style.display = "none";
+		document.getElementById("NWireless").style.display = "none";
+			document.getElementById("SetupWirelessNW").style.display = "none";
+}
